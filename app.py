8b19d71d4bdfcdabc23e786b0994dbceeceacb8e
emre kaan <emre.usta@sabanciuniv.edu>
2023-12-28 16:33:29 +0300

    year option is added. new datasets are created

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import os
 app = Flask(__name__, static_folder='static')
 
 # Assume you have a function to fetch top listened data for each category
-def fetch_top_listened_data(category):
-    file_path = f'top_listened_{category}_data.csv'
-    if os.path.exists(file_path):
+
+def fetch_top_listened_data(category, year):
+    # Adjust file_path based on the selected year
+    if year == 'all':
+        file_path = f'top_listened_{category}_data.csv'
         df = pd.read_csv(file_path)
         # Convert DataFrame to a list of lists
         data_list = df.values.tolist()
@@ -16,27 +18,102 @@ def fetch_top_listened_data(category):
         data_list.insert(0, header)
         return data_list
     else:
-        return []
-    
+        file_path = f'top_listened_{category}_data_{year}.csv'
+        df = pd.read_csv(file_path)
+        # Convert DataFrame to a list of lists
+        data_list = df.values.tolist()
+        # Add column names as the first row
+        header = df.columns.tolist()
+        data_list.insert(0, header)
+        return data_list
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#all
+
 @app.route('/artists')
 def artists():
-    top_listened_artists = fetch_top_listened_data('artists')
+    top_listened_artists = fetch_top_listened_data('artists', "all")
     return render_template('artists.html', top_listened_artists=top_listened_artists)
 
 @app.route('/songs')
 def songs():
-    top_listened_songs = fetch_top_listened_data('songs')
+    top_listened_songs = fetch_top_listened_data('songs', "all")
     return render_template('songs.html', top_listened_songs=top_listened_songs)
 
 @app.route('/albums')
 def albums():
-    top_listened_albums = fetch_top_listened_data('albums')
+    top_listened_albums = fetch_top_listened_data('albums', "all")
     return render_template('albums.html', top_listened_albums=top_listened_albums)
+
+#2021
+
+@app.route('/2021')
+def index2021():
+    return render_template('index-2021.html')
+
+@app.route('/artists-2021')
+def artists2021():
+    top_listened_artists = fetch_top_listened_data('artists', "2021")
+    return render_template('artists-2021.html', top_listened_artists=top_listened_artists)
+
+@app.route('/songs-2021')
+def songs2021():
+    top_listened_songs = fetch_top_listened_data('songs', "2021")
+    return render_template('songs-2021.html', top_listened_songs=top_listened_songs)
+
+@app.route('/albums-2021')
+def albums2021():
+    top_listened_albums = fetch_top_listened_data('albums', "2021")
+    return render_template('albums-2021.html', top_listened_albums=top_listened_albums)
+
+#2022
+
+@app.route('/2022')
+def index2022():
+    return render_template('index-2022.html')
+
+@app.route('/artists-2022')
+def artists2022():
+    top_listened_artists = fetch_top_listened_data('artists', "2022")
+    return render_template('artists-2022.html', top_listened_artists=top_listened_artists)
+
+@app.route('/songs-2022')
+def songs2022():
+    top_listened_songs = fetch_top_listened_data('songs', "2022")
+    return render_template('songs-2022.html', top_listened_songs=top_listened_songs)
+
+@app.route('/albums-2022')
+def albums2022():
+    top_listened_albums = fetch_top_listened_data('albums', "2022")
+    return render_template('albums-2022.html', top_listened_albums=top_listened_albums)
+
+#2023
+
+@app.route('/2023')
+def index2023():
+    return render_template('index-2023.html')
+
+
+@app.route('/artists-2023')
+def artists2023():
+    top_listened_artists = fetch_top_listened_data('artists', "2023")
+    return render_template('artists-2023.html', top_listened_artists=top_listened_artists)
+
+@app.route('/songs-2023')
+def songs2023():
+    top_listened_songs = fetch_top_listened_data('songs', "2023")
+    return render_template('songs-2023.html', top_listened_songs=top_listened_songs)
+
+@app.route('/albums-2023')
+def albums2023():
+    top_listened_albums = fetch_top_listened_data('albums', "2023")
+    return render_template('albums-2023.html', top_listened_albums=top_listened_albums)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -58,3 +58,204 @@ top_listened_albums_df.to_csv('top_listened_albums_data.csv', index=False)
 print(f'Top 10 Listened Artists:\n{top_listened_artists}')
 print(f'Top 10 Listened Songs:\n{top_listened_songs}')
 print(f'Top 10 Listened Albums:\n{top_listened_albums}')
+
+
+
+
+
+
+#2021
+
+
+
+
+
+json_files = ["1.json"]
+
+# Initialize an empty DataFrame to store the combined data
+combined_df = pd.DataFrame()
+
+# Loop through each JSON file and concatenate the data
+for file in json_files:
+    df = pd.read_json(file)
+    combined_df = pd.concat([combined_df, df], ignore_index=True)
+
+# Convert 'ts' column to datetime format
+combined_df['ts'] = pd.to_datetime(combined_df['ts'])
+
+# Calculate listening time for each artist, song, genre, album
+listening_time_artists = combined_df.groupby('master_metadata_album_artist_name')['ms_played'].sum() / (60 * 1000)
+listening_time_songs = combined_df.groupby('master_metadata_track_name')['ms_played'].sum() / (60 * 1000)
+listening_time_genres = combined_df.groupby('master_metadata_album_artist_name')['ms_played'].sum() / (60 * 1000)
+listening_time_albums = combined_df.groupby('master_metadata_album_album_name')['ms_played'].sum() / (60 * 1000)
+
+# Get the top 10 listening times for each category
+top_listened_artists = listening_time_artists.nlargest(10).reset_index()
+top_listened_songs = listening_time_songs.nlargest(10).reset_index()
+top_listened_genres = listening_time_genres.nlargest(10).reset_index()
+top_listened_albums = listening_time_albums.nlargest(10).reset_index()
+
+# Round the values to two decimal places
+top_listened_artists['ms_played'] = top_listened_artists['ms_played'].round(2)
+top_listened_songs['ms_played'] = top_listened_songs['ms_played'].round(2)
+top_listened_genres['ms_played'] = top_listened_genres['ms_played'].round(2)
+top_listened_albums['ms_played'] = top_listened_albums['ms_played'].round(2)
+
+# Create DataFrames for the top listened data
+top_listened_artists_df = pd.DataFrame({
+    'Top Listened Artists': top_listened_artists['master_metadata_album_artist_name'],
+    'Listening Time (minutes)': top_listened_artists['ms_played']
+})
+
+top_listened_songs_df = pd.DataFrame({
+    'Top Listened Songs': top_listened_songs['master_metadata_track_name'],
+    'Listening Time (minutes)': top_listened_songs['ms_played']
+})
+
+
+top_listened_albums_df = pd.DataFrame({
+    'Top Listened Albums': top_listened_albums['master_metadata_album_album_name'],
+    'Listening Time (minutes)': top_listened_albums['ms_played']
+})
+
+# Save the DataFrames to CSV files
+top_listened_artists_df.to_csv('top_listened_artists_data_2021.csv', index=False)
+top_listened_songs_df.to_csv('top_listened_songs_data_2021.csv', index=False)
+top_listened_albums_df.to_csv('top_listened_albums_data_2021.csv', index=False)
+
+# Print or use the information as needed
+print(f'Top 10 Listened Artists:\n{top_listened_artists}')
+print(f'Top 10 Listened Songs:\n{top_listened_songs}')
+print(f'Top 10 Listened Albums:\n{top_listened_albums}')
+
+
+
+
+
+#2022 
+
+
+
+
+json_files = ["3.json"]
+
+# Initialize an empty DataFrame to store the combined data
+combined_df = pd.DataFrame()
+
+# Loop through each JSON file and concatenate the data
+for file in json_files:
+    df = pd.read_json(file)
+    combined_df = pd.concat([combined_df, df], ignore_index=True)
+
+# Convert 'ts' column to datetime format
+combined_df['ts'] = pd.to_datetime(combined_df['ts'])
+
+# Calculate listening time for each artist, song, genre, album
+listening_time_artists = combined_df.groupby('master_metadata_album_artist_name')['ms_played'].sum() / (60 * 1000)
+listening_time_songs = combined_df.groupby('master_metadata_track_name')['ms_played'].sum() / (60 * 1000)
+listening_time_genres = combined_df.groupby('master_metadata_album_artist_name')['ms_played'].sum() / (60 * 1000)
+listening_time_albums = combined_df.groupby('master_metadata_album_album_name')['ms_played'].sum() / (60 * 1000)
+
+# Get the top 10 listening times for each category
+top_listened_artists = listening_time_artists.nlargest(10).reset_index()
+top_listened_songs = listening_time_songs.nlargest(10).reset_index()
+top_listened_genres = listening_time_genres.nlargest(10).reset_index()
+top_listened_albums = listening_time_albums.nlargest(10).reset_index()
+
+# Round the values to two decimal places
+top_listened_artists['ms_played'] = top_listened_artists['ms_played'].round(2)
+top_listened_songs['ms_played'] = top_listened_songs['ms_played'].round(2)
+top_listened_genres['ms_played'] = top_listened_genres['ms_played'].round(2)
+top_listened_albums['ms_played'] = top_listened_albums['ms_played'].round(2)
+
+# Create DataFrames for the top listened data
+top_listened_artists_df = pd.DataFrame({
+    'Top Listened Artists': top_listened_artists['master_metadata_album_artist_name'],
+    'Listening Time (minutes)': top_listened_artists['ms_played']
+})
+
+top_listened_songs_df = pd.DataFrame({
+    'Top Listened Songs': top_listened_songs['master_metadata_track_name'],
+    'Listening Time (minutes)': top_listened_songs['ms_played']
+})
+
+
+top_listened_albums_df = pd.DataFrame({
+    'Top Listened Albums': top_listened_albums['master_metadata_album_album_name'],
+    'Listening Time (minutes)': top_listened_albums['ms_played']
+})
+
+# Save the DataFrames to CSV files
+top_listened_artists_df.to_csv('top_listened_artists_data_2022.csv', index=False)
+top_listened_songs_df.to_csv('top_listened_songs_data_2022.csv', index=False)
+top_listened_albums_df.to_csv('top_listened_albums_data_2022.csv', index=False)
+
+# Print or use the information as needed
+print(f'Top 10 Listened Artists:\n{top_listened_artists}')
+print(f'Top 10 Listened Songs:\n{top_listened_songs}')
+print(f'Top 10 Listened Albums:\n{top_listened_albums}')
+
+
+
+
+#2023 
+
+
+
+json_files = ["5.json"]
+
+# Initialize an empty DataFrame to store the combined data
+combined_df = pd.DataFrame()
+
+# Loop through each JSON file and concatenate the data
+for file in json_files:
+    df = pd.read_json(file)
+    combined_df = pd.concat([combined_df, df], ignore_index=True)
+
+# Convert 'ts' column to datetime format
+combined_df['ts'] = pd.to_datetime(combined_df['ts'])
+
+# Calculate listening time for each artist, song, genre, album
+listening_time_artists = combined_df.groupby('master_metadata_album_artist_name')['ms_played'].sum() / (60 * 1000)
+listening_time_songs = combined_df.groupby('master_metadata_track_name')['ms_played'].sum() / (60 * 1000)
+listening_time_genres = combined_df.groupby('master_metadata_album_artist_name')['ms_played'].sum() / (60 * 1000)
+listening_time_albums = combined_df.groupby('master_metadata_album_album_name')['ms_played'].sum() / (60 * 1000)
+
+# Get the top 10 listening times for each category
+top_listened_artists = listening_time_artists.nlargest(10).reset_index()
+top_listened_songs = listening_time_songs.nlargest(10).reset_index()
+top_listened_genres = listening_time_genres.nlargest(10).reset_index()
+top_listened_albums = listening_time_albums.nlargest(10).reset_index()
+
+# Round the values to two decimal places
+top_listened_artists['ms_played'] = top_listened_artists['ms_played'].round(2)
+top_listened_songs['ms_played'] = top_listened_songs['ms_played'].round(2)
+top_listened_genres['ms_played'] = top_listened_genres['ms_played'].round(2)
+top_listened_albums['ms_played'] = top_listened_albums['ms_played'].round(2)
+
+# Create DataFrames for the top listened data
+top_listened_artists_df = pd.DataFrame({
+    'Top Listened Artists': top_listened_artists['master_metadata_album_artist_name'],
+    'Listening Time (minutes)': top_listened_artists['ms_played']
+})
+
+top_listened_songs_df = pd.DataFrame({
+    'Top Listened Songs': top_listened_songs['master_metadata_track_name'],
+    'Listening Time (minutes)': top_listened_songs['ms_played']
+})
+
+
+top_listened_albums_df = pd.DataFrame({
+    'Top Listened Albums': top_listened_albums['master_metadata_album_album_name'],
+    'Listening Time (minutes)': top_listened_albums['ms_played']
+})
+
+# Save the DataFrames to CSV files
+top_listened_artists_df.to_csv('top_listened_artists_data_2023.csv', index=False)
+top_listened_songs_df.to_csv('top_listened_songs_data_2023.csv', index=False)
+top_listened_albums_df.to_csv('top_listened_albums_data_2023.csv', index=False)
+
+# Print or use the information as needed
+print(f'Top 10 Listened Artists:\n{top_listened_artists}')
+print(f'Top 10 Listened Songs:\n{top_listened_songs}')
+print(f'Top 10 Listened Albums:\n{top_listened_albums}')
